0de574c04f9a7ac082075797776b747a10471f87
Jsl <100409086+Afx-Abu@users.noreply.github.com>
2024-04-06 12:52:07 +0530

    Update welcome.js

--- plugins/welcome.js
+++ plugins/welcome.js
@@ -1,6 +1,6 @@
 const {
     Module,
-    groupDB,
+    getWelcome,
     config
 } = require('../lib')
 
@@ -11,7 +11,7 @@ Module({
     fromMe: true,
     onlyGroup: true
 }, async (message, match) => {
-	const {welcome} = await groupDB(['welcome'], {jid: message.jid, content: {}}, 'get');
+	const {welcome} = await getWelcome(['welcome'], {jid: message.jid, content: {}}, 'get');
     if(match.toLowerCase() == 'get'){
         const status = welcome && welcome.status ? welcome. status : 'false';
         if(status == 'false') return await message.reply(`_*Example:* welcome get_\n_welcome hy &mention_`);
@@ -20,16 +20,16 @@ Module({
     } else if(match.toLowerCase() == 'off'){
         const status = welcome && welcome.status ? welcome. status : 'false';
         if(status == 'false') return await message.reply(`_Welcome already deactivated_`);
-        await groupDB(['welcome'], {jid: message.jid, content: {status: 'false',message: welcome.message }}, 'set');
+        await getWelcome(['welcome'], {jid: message.jid, content: {status: 'false',message: welcome.message }}, 'set');
         return await message.reply('_Welcome Disabled✅_');
     } else if(match.toLowerCase() == 'on'){
         const status = welcome && welcome.status ? welcome. status : 'false';
         if(status == 'true') return await message.reply(`_Welcome already activated_`);
-        await groupDB(['welcome'], {jid: message.jid, content: {status: 'true',message: welcome.message }}, 'set');
+        await getWelcome(['welcome'], {jid: message.jid, content: {status: 'true',message: welcome.message }}, 'set');
         return await message.reply('_Welcome Enabled✅_');
     } else if(match){
         const status = welcome && welcome.status ? welcome. status : 'false';
-        await groupDB(['welcome'], {jid: message.jid, content: {status, message: match}}, 'set');
+        await getWelcome(['welcome'], {jid: message.jid, content: {status, message: match}}, 'set');
         return await message.reply('_Welcome set Successfuly_');
     }
     return await message.reply('_*welcome get*_\n_*welcome* thank you for joining &mention_\n*_welcome false_*');
@@ -42,7 +42,7 @@ Module({
     fromMe: true,
     onlyGroup: true
 }, async (message, match) => {
-	const {exit} = await groupDB(['exit'], {jid: message.jid, content: {}}, 'get');
+	const {exit} = await getWelcome(['exit'], {jid: message.jid, content: {}}, 'get');
     if(match.toLowerCase() == 'get'){
         const status = exit && exit.status ? exit.status : 'false';
         if(status == 'false') return await message.reply(`_*Example:* goodbye get_\n_goodbye hy &mention\n_*for more:* visit ${config.BASE_URL}info/goodbye_`);
@@ -51,18 +51,19 @@ Module({
     } else if(match.toLowerCase() == 'off'){
         const status = exit && exit.status ? exit.status : 'false';
         if(status == 'false') return await message.reply(`_Goodbye already activated_`);
-        await groupDB(['exit'], {jid: message.jid, content: {status: 'false',message: exit.message }}, 'set');
+        await getWelcome(['exit'], {jid: message.jid, content: {status: 'false',message: exit.message }}, 'set');
         return await message.reply('_Goodbye Disabled_');
     } else if(match.toLowerCase() == 'on'){
         const status = exit && exit.status ? exit.status : 'false';
         if(status == 'true') return await message.reply(`_Goodbye already deactivated_`);
-        await groupDB(['exit'], {jid: message.jid, content: {status: 'true',message: exit.message }}, 'set');
+        await getWelcome(['exit'], {jid: message.jid, content: {status: 'true',message: exit.message }}, 'set');
         return await message.reply('_Goodbye Enabled_');
     } else if(match){
         const status = exit && exit.status ? exit.status : 'false';
-        await groupDB(['exit'], {jid: message.jid, content: {status, message: match}}, 'set');
+        await getWelcome(['exit'], {jid: message.jid, content: {status, message: match}}, 'set');
         return await message.reply('_Goodbye Set Successfully_');
     }
     return await message.reply('_*goodbye get*_\n_*goodbye* Hy bro &mention_\n*_goodbye false_*');
 });
+	      
 	      
