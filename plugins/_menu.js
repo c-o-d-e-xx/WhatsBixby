a7756b12d946ee9ac49eee4dc1d8a27e3b9d918e
Jsl <100409086+Afx-Abu@users.noreply.github.com>
2024-04-06 11:55:43 +0530

    Update _menu.js

--- plugins/_menu.js
+++ plugins/_menu.js
@@ -1,56 +1,65 @@
 const {
     Module,
     commands,
-    send_alive,
-    send_menu,
-    tiny,
-    lang,
-    personalDB,
-    isPublic
-} = require('../lib')
+    sendMenu,
+    getAlive,
+    sendAlive,
+    isPublic,
+    tiny
+} = require('../lib/')
 
 Module({
-	pattern: 'list',
-	desc: lang.LIST.DESC,
-	type: 'info',
-	fromMe: isPublic
-}, async (message) => {
-	let count = 1,
-		list = "";
-	commands.map((cmd => {
-		if (cmd.pattern && cmd.desc) {
-			list += `${count++} : *${cmd.pattern.replace(/[^a-zA-Z0-9,-]/g,"")}*\n_${cmd.desc}_\n\n`;
-		} else {
-			list += `${count++} *${cmd.pattern?cmd.pattern.replace(/[^a-zA-Z0-9,-]/g,""):''}*\n`
-		}
-	}));
-	var jsl = await tiny(list)
-	return await message.reply(jsl);
+    pattern: 'ping ?(.*)',
+    desc: 'Send Bot Status',
+    fromMe: isPublic,
+    type: 'info'
+}, async (message, match) => {
+    var start = new Date().getTime();
+    var msg = await message.reply('*Testing Speed..*');
+    var end = new Date().getTime();
+    await message.reply('⟪ *Response in ' + (end - start) + ' msec* ⟫');
 });
 
 Module({
     pattern: "menu",
-    desc: lang.MENU.DESC,
-    type: 'whatsapp',
+    desc: 'Send Bot Menu',
+    type: 'info',
     fromMe: isPublic
 }, async (message, match) => {
-    return await send_menu(message, 'non button');
+    return await sendMenu(message, 'non button');
 });
 
 Module({
     pattern: "alive",
-    desc: lang.ALIVE.DESC,
+    desc: 'Send Bot Alive Message',
     type: 'info',
-    usage:lang.ALIVE.HELP,
     fromMe: isPublic
 }, async (message, match) => {
     if(match == "get" && message.isCreator){
-	    const {alive} = await personalDB(['alive'], {content:{}},'get');
-	    return await message.send(alive);
-    } else if(match && message.isCreator){
-	    await personalDB(['alive'], {content: match},'set');
+	    const {alive} = await getAlive(['alive'], {content:{}},'get');
+	    return await message.reply(alive);} else if(match && message.isCreator){
+	    await getAlive(['alive'], {content: match},'set');
 	    return await message.reply('_Alive Message Updated_');
     }
-    const {alive} = await personalDB(['alive'], {content:{}},'get');
-    return await send_alive(message, alive);
+    const {alive} = await getAlive(['alive'], {content:{}},'get');
+    return await sendAlive(message, alive);
 });
+
+
+Module({
+	pattern: 'list',
+	desc: 'Send Bot List',
+	type: 'info',
+	fromMe: isPublic
+}, async (message) => {
+	let count = 1,
+	list = "";
+	commands.map((cmd => {if (cmd.pattern && cmd.desc) {list += `${count++} *${cmd.pattern.replace(/[^a-zA-Z0-9,-]/g,"")}*\n_${cmd.desc}_\n\n`;
+		} else {
+		list += `${count++} : *${cmd.pattern?cmd.pattern.replace(/[^a-zA-Z0-9,-]/g,""):''}*\n`
+		}
+	}));
+	const listt = await tiny(list)
+	return await message.reply(listt);
+});
+
