97efa8f24ad13a31a0c69887e2c080487211c1d3
Jsl <100409086+Afx-Abu@users.noreply.github.com>
2024-04-06 12:56:45 +0530

    Update whatsapp.js

--- plugins/whatsapp.js
+++ plugins/whatsapp.js
@@ -4,9 +4,7 @@ const {
 	getCompo,
 	sleep,
 	lang,
-	config,
-	isAdmin,
-	isBotAdmin
+	config
 } = require('../lib');
 const {
 	WA_DEFAULT_EPHEMERAL
@@ -133,7 +131,7 @@ Module({
 	pattern: 'jid',
 	fromMe: isPublic,
 	desc: lang.USER.JID,
-	type: "general"
+	type: "user"
 }, async (message) => {
 	if (message.reply_message.sender) {
 		await message.reply(message.reply_message.sender)
@@ -338,7 +336,8 @@ Module({
 		};
 	}
 	await message.send(img, {
-		caption: msg, quoted: message
+		caption: msg,
+		quoted: message
 	}, 'image');
 })
 Module({
@@ -411,5 +410,40 @@ Module({
 	const available_privacy = ['all', 'contacts', 'contact_blacklist', 'none'];
 	if (!available_privacy.includes(match)) return await message.reply(`_action must be *${available_privacy.join('/')}* values_`);
 	await message.client.updateGroupsAddPrivacy(match)
-	await message.reply(`_Privacy Updated to *${match}*_`);
+	await message.send(`_Privacy Updated to *${match}*_`);
+})
+
+
+
+Module({
+        pattern: 'whois ?(.*)',
+        fromMe: isPublic,
+        type: 'info',
+        desc: 'get user bio and image'
+}, async (message, match) => {
+                let user = (message.reply_message.sender || match).replace(/[^0-9]/g, '');
+                if (!user) return message.reply('_Need a User!_')
+                user += '@s.whatsapp.net';
+                try {
+                        pp = await message.client.profilePictureUrl(user, 'image')
+                } catch {
+                        pp = 'https://i.imgur.com/b3hlzl5.jpg'
+                }
+                let status = await message.client.fetchStatus(user)
+                const date = new Date(status.setAt);
+                const options = {
+                        year: 'numeric',
+                        month: 'long',
+                        day: 'numeric',
+                        hour: 'numeric',
+                        minute: 'numeric',
+                        second: 'numeric'
+                };
+                const setAt = date.toLocaleString('en-US', options);
+                await message.send({
+                        url: pp
+                }, {
+                        caption: `*Name :* ${await message.getName(user)}\n*About :* ${status.status}\n*About Set Date :* ${setAt}`,
+                        quoted: message.data
+                }, 'image')
 })
