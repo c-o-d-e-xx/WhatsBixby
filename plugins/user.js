f17e73781cd88a5dc52bcd578d40f76c4e7744ac
Jsl <100409086+Afx-Abu@users.noreply.github.com>
2024-04-06 15:13:34 +0530

    Update user.js

--- plugins/user.js
+++ plugins/user.js
@@ -13,8 +13,8 @@ Module({
     fromMe :true,
     media: "sticker"//you can get this type of active action from 'eval'=>() return lib.commands[0]
 }, async (message, match) => {
-    if (!message.reply_message.msg?.fileSha256) return message.send(lang.MEDIA_CMD.CMD_ERROR)
-    if (!match) return await message.send(lang.MEDIA_CMD.NO_CMD)
+    if (!message.reply_message.msg?.fileSha256) return message.reply(lang.MEDIA_CMD.CMD_ERROR)
+    if (!match) return await message.reply(lang.MEDIA_CMD.NO_CMD)
     await getVar(['sticker_cmd'], {content:{[match]: message.reply_message.msg.fileSha256.join("")}},'add');
     return await message.reply(lang.BASE.SUCCESS)
 });
@@ -24,7 +24,7 @@ Module({
     type: "action",
     fromMe :true
 }, async (message, match) => {
-    if (!match) return await message.send(lang.MEDIA_CMD.NO_CMD)
+    if (!match) return await message.reply(lang.MEDIA_CMD.NO_CMD)
     await getVar(['sticker_cmd'], {content:{id: match}},'delete');
     return await message.reply(lang.BASE.SUCCESS)
 });
@@ -35,7 +35,7 @@ Module({
     fromMe :true
 }, async (message, match) => {
     const {sticker_cmd} = await getVar(['sticker_cmd'], {content:{}},'get');
-    if(!Object.keys(sticker_cmd)[0]) return await message.send(lang.MEDIA_CMD.NOT_FOUND);
+    if(!Object.keys(sticker_cmd)[0]) return await message.reply(lang.MEDIA_CMD.NOT_FOUND);
     let cmds = lang.MEDIA_CMD.CMD_LIST+'\n\n';
     let n = 1;
     for(const cmd in sticker_cmd) {
