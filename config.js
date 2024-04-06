935a187d9fccbf66d019211601c82c8e76d10c33
Jsl <100409086+Afx-Abu@users.noreply.github.com>
2024-04-06 11:47:03 +0530

    Update config.js

--- config.js
+++ config.js
@@ -9,9 +9,6 @@ module.exports = {
     HEROKU: {
     API_KEY: process.env.HEROKU_API_KEY,
     APP_NAME: process.env.HEROKU_APP_NAME },
-    PORT: process.env.PORT || 3000,
-    BASE_URL : "https://upper-romy-inrl-bot.koyeb.app/",
-    REPO: "Afx-Abu/Abu-MD",
     BGM_URL : process.env.BGM_URL || "null",
     REJECT_CALL : toBool(process.env.REJECT_CALL || 'false'),
     BADWORD_BLOCK : toBool(process.env.BADWORD_BLOCK || 'false'),
@@ -25,21 +22,20 @@ module.exports = {
     DISABLE_PM: toBool(process.env.DISABLE_PM || "false"),
     DISABLE_GRP : toBool(process.env.DISABLE_GRP || "false"),
     ERROR_MSG : toBool(process.env.ERROR_MSG || "true"),
-    AJOIN: toBool(process.env.AJOIN || 'false'),
-    READ : process.env.READ ||  "false",//true, command
+    GPJOIN: toBool(process.env.GPJOIN || 'false'),
+    AUTO_READ : process.env.AUTO_READ ||  "false",//true, command
     CHATBOT : process.env.CHATBOT || "false",//true, pm, group
-    REACT : process.env.REACT || "false",//true, command, emoji
+    AUTO_REACT : process.env.AUTO_REACT || "false",//true, command, emoji
     WARNCOUND : process.env.WARNCOUND || 5,
-    BOT_INFO : process.env.BOT_INFO || "Abu MD;Jsl;https://i.imgur.com/DyLAuEh.jpg",
+    BOT_INFO : process.env.BOT_INFO || "Abu MD;Jsl;https://i.imgur.com/o3WP9EK.jpeg",
     MODE : process.env.MODE || "private",
     PREFIX : process.env.PREFIX || "[.,!]",//both  .  and [.] equal, for multi prefix we use [] this
     LANG : process.env.LANG || "en",
-    PERSONAL_MESSAGE: process.env.PERSONAL_MESSAGE || "null",
+    PM_MESSAGE: process.env.PM_MESSAGE || "null",
     BOT_PRESENCE : process.env.BOT_PRESENCE || "unavailable",
     AUDIO_DATA : process.env.AUDIO_DATA || "ABU MD;JSL;https://i.imgur.com/DyLAuEh.jpg",
     STICKER_DATA : process.env.STICKER_DATA || "ᴀʙᴜ-ᴍᴅ;ᴊsʟ",
-    BRAINSHOP: process.env.BRAINSHOP || '172372,nbjE0YAlyw3cpoMl',
-    SUDO : process.env.SUDO || "917025994178",
+    SUDO : process.env.SUDO || "918943027806",
     RMBG_KEY: process.env.RMBG_KEY,
     OPEN_AI: process.env.OPEN_AI,
     ELEVENLABS: process.env.ELEVENLABS,
