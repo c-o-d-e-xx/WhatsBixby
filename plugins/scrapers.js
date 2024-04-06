05c3b2e4ba5e8450372807c97f7003b51728d662
Jsl <100409086+Afx-Abu@users.noreply.github.com>
2024-04-06 13:19:33 +0530

    Update scrapers.js

--- plugins/scrapers.js
+++ plugins/scrapers.js
@@ -1,30 +1,4 @@
 const { TRT, TTS, isPublic, Module, lang } = require('../lib');
-const jimp = require('jimp');
-const QRReader = require('qrcode-reader')
-
-Module({
-        pattern: 'qr ?(.*)',
-        fromMe: isPublic,
-        desc: 'qr code reader & generater',
-        type: 'plugin'
-}, async (message, match) => {
-        match = match || message.reply_message.text;
-        if (!message.reply_message.image && !match) return await message.reply("_Reply to a qr image/text message_")
-        if (message.reply_message.image) {
-                const {
-                        bitmap
-                } = await jimp.read(await message.reply_message.download())
-                const qr = new QRReader()
-                qr.callback = (err, value) => message.reply(err ?? value.result)
-                qr.decode(bitmap)
-        } else {
-                        return await message.sendReply(config.BASE_URL + 'api/qrcode?text=' + match, {
-                                caption: "*result for* ```" + match + "```",
-			        quoted: message 
-                        }, "image");
-        }
-});
-
 
 Module(
 	{
