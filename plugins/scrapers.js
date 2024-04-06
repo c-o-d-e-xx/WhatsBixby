9ce559f67b34c44d559c3be80bb4e99c1324ff65
Jsl <100409086+Afx-Abu@users.noreply.github.com>
2024-04-06 13:01:18 +0530

    Update scrapers.js

--- plugins/scrapers.js
+++ plugins/scrapers.js
@@ -1,15 +1,28 @@
-const {
-    Module,
-    isPublic,
-    TTS,
-    lang,
-    TRT
-} = require('../lib');
+const { TRT, TTS, isPublic, Module, lang } = require('../lib');
+
+Module(
+	{
+		pattern: 'trt ?(.*)',
+		fromMe: isPublic,
+		desc: 'langauge Changer',
+		type: 'converter',
+	},
+	async (message, match) => {
+		if (!message.reply_message.text)
+			return await message.reply(
+				lang.TRT.NEED
+			)
+                if(!match) return await message.reply(lang.TRT.NEED_LANG);
+                const {text} = await TRT(message.reply_message.text, match)
+		return await message.reply(text);
+	}
+)
+
 
 Module({
     pattern: 'tts',
     fromMe: isPublic,
-    desc: lang.TTS_DESC,
+    desc: 'Convert ypur text to audio',
     type: "converter"
 }, async (message, match) => {
         match = match || message.reply_message.text;
@@ -23,25 +36,9 @@ Module({
         return await message.send(await TTS(match,lang),{
             mimetype: 'audio/ogg; codecs=opus',
             ptt: false,
-            quoted: message
+            quoted: message 
         },'audio');
 });
 
 
-Module(
-	{
-		pattern: 'trt ?(.*)',
-		fromMe: isPublic,
-		desc: lang.TRT.DESC,
-		type: 'search',
-	},
-	async (message, match) => {
-		if (!message.reply_message.text)
-			return await message.reply(
-				lang.TRT.NEED
-			)
-                if(!match) return await message.reply(lang.TRT.NEED_LANG);
-                const {text} = await TRT(message.reply_message.text, match)
-		return await message.reply(text);
-	}
-)
+
